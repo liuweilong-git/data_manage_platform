59ba006a94d47c6db28fb1198cb261944c4276bf
学无止境 <7536456+CSaCA@user.noreply.gitee.com>
2024-07-29 23:32:27 +0800

    第一次提交

---
+++ src/main/java/com/github/code/data_manage_platform/DataManagePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.github.code.data_manage_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataManagePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataManagePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/code/data_manage_platform/DataManagePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.code.data_manage_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataManagePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
